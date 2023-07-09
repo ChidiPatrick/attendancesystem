54445c8705fbc2c24f3188f693332dbbdd108563
Patrick Chidi <okaforpatrick302@gmail.com>
2023-07-09 20:53:38 +0100

    First commit in attendance system  project

---
+++ Firebase/firebase.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+const firebaseConfig = {
+  apiKey: "AIzaSyDsDLd94I9o72t0b8YWa84DpZNWUdCRfQo",
+  authDomain: "attendancesytem-a7992.firebaseapp.com",
+  projectId: "attendancesytem-a7992",
+  storageBucket: "attendancesytem-a7992.appspot.com",
+  messagingSenderId: "59931528945",
+  appId: "1:59931528945:web:c008682dc2f15acdd3a350",
+  measurementId: "G-HXPMJ0C2JL",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
---
+++ src/Components/Store/store.js
@@ -0,0 +1,4 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+export const Store = configureStore({
+  reducer: {},
+});
