edd22af2d7983d174fb6f3f05b1785bed6098262
Kingsley Oyeoka <kingstoneoy@gmail.com>
2023-07-28 13:47:45 +0100

    header conflict resolved

--- src/Components/LandingPageComponents/Header/header.js
+++ src/Components/LandingPageComponents/Header/header.js
@@ -25,7 +25,6 @@ const Header = () => {
     navigate("/signin");
   };
   return (
-<<<<<<< HEAD
     <header className=" w-full py-6">
       <div className=" w-[90%] md:w-[80%] mx-auto">
         <h1 className="flex flex-col">
@@ -42,34 +41,12 @@ const Header = () => {
           being present in the moment.
         </p>
         <div className="w-full flex place-content-center mt-5">
-          {/* <img src="./images/herolIlustrator.svg" alt="illustrator image" /> */}
+          <img src="./images/herolIlustrator.svg" alt="illustrator image" />
         </div>
         <div className="mt-10 mx-auto flex justify-between w-full gap-3">
           <ButtonFull handleClick={signupBtnClickHandler}>Sign Up</ButtonFull>
           <ButtonLight handleSignin={signinBtnClickHandler}>Log In</ButtonLight>
         </div>
-=======
-    <header className="p-2">
-      <h1 className="flex flex-col">
-        <span className="mt-3 text-md font-bold text-center text-lp-font-color-300">
-          GPS ENABLED
-        </span>
-        <span className="text-2xl text-center text-lp-primary font-bold my-4">
-          ATTENDANCE MANAGEMENT SYSTEM
-        </span>
-      </h1>
-      <p className="text-center mt-4 text-lp-font-color">
-        Attendance made easy, with a touch of technology! Let our GPS and facial
-        recognition technology work their wonders, while you focus on being
-        present in the moment.
-      </p>
-      <div className="w-full flex place-content-center mt-5">
-        <img src="./images/herolIlustrator.svg" alt="illustrator image" />
-      </div>
-      <div className="mt-10 mx-auto flex justify-between w-full">
-        <ButtonFull handleClick={signupBtnClickHandler}>Sign Up</ButtonFull>
-        <ButtonLight handleSignin={signinBtnClickHandler}>Log in</ButtonLight>
->>>>>>> origin/main
       </div>
     </header>
   );
